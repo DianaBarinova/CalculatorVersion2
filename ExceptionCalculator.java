750f2632725798c4ed0b462fcdfc916c80c4a0c2
Diana Barinova <93100344+DianaBarinova@users.noreply.github.com>
2022-11-09 23:53:36 +0300

    Delete ExceptionCalculator.java

--- ExceptionCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.example.restservice;
-
-public class ExceptionCalculator extends RuntimeException {
-    ExceptionCalculator (String operation){
-        super("Could not find operation " + operation);
-    }
-}
