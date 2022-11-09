c495e023fe7801786696c9e397f1d8e6634a25f9
Diana Barinova <93100344+DianaBarinova@users.noreply.github.com>
2022-11-09 11:09:03 +0300

    Add files via upload

---
+++ CalculatorAction.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+public class CalculatorAction {
+    public static int ADD(int a, int b) {
+        int answer = a + b;
+        return answer;
+    }
+    public static int DIF(int a, int b) {
+        int answer = a - b;
+        return answer;
+    }
+    public static int MUL(int a, int b) {
+        int answer = a * b;
+        return answer;
+    }
+    public static int DIV(int a, int b) {
+        if(b == 0){
+            throw new IllegalArgumentException();
+        }
+
+            int answer = a / b;
+
+        return answer;
+    }
+}
